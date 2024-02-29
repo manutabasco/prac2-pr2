5b44e709caacf69e7330106983058d3596017d9d
Manuel Tabasco <manuel.tabasco@usp.ceu.es>
2024-02-29 12:12:51 +0100

    package info

---
+++ src/aplicacion/package_info.java
@@ -0,0 +1,7 @@
+package aplicacion;
+/**
+ * El paquete `aplicacion` contiene la clase `Principal`, que presenta un programa principal en Java.
+ * Este programa utiliza la clase `Matematicas` del paquete `mates`
+ * para calcular una aproximación del número pi mediante el método de Montecarlo.
+ * El valor de pi se imprime en la consola al ejecutar el programa, tomando como entrada el número de pasos para la aproximación.
+ */
---
+++ src/mates/package_info.java
@@ -0,0 +1,5 @@
+package mates;
+/**
+ * El paquete `mates` contiene la clase `Matematicas`, que implementa el método `generarNumeroPiRecursivo`.
+ * Proporciona un método estático para generar una aproximación del número pi utilizando el método de Montecarlo de forma recursiva.
+ */
